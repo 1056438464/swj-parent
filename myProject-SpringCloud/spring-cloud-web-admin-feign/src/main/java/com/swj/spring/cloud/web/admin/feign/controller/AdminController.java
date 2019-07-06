4db970833fac18667cc2321642b16c9e907edc2c
jwsssssssss <1056438464@qq.com>
2019-07-06 21:36:54 +0800

    feign 消费者

--- myProject-SpringCloud/spring-cloud-web-admin-feign/src/main/java/com/swj/spring/cloud/web/admin/feign/controller/AdminController.java
+++ myProject-SpringCloud/spring-cloud-web-admin-feign/src/main/java/com/swj/spring/cloud/web/admin/feign/controller/AdminController.java
@@ -1,6 +1,6 @@
-package com.swj.spring.cloud.web.admin.ribbon.controller;
+package com.swj.spring.cloud.web.admin.feign.controller;
 
-import com.swj.spring.cloud.web.admin.ribbon.service.AdminService;
+import com.swj.spring.cloud.web.admin.feign.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
