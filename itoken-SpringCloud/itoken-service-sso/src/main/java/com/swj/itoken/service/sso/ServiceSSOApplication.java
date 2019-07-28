cacb6883919823eb34f5650b2611788f50428ca3
jwsssssssss <1056438464@qq.com>
2019-07-28 18:10:05 +0800

    sso 拦截器

--- itoken-SpringCloud/itoken-service-sso/src/main/java/com/swj/itoken/service/sso/ServiceSSOApplication.java
+++ itoken-SpringCloud/itoken-service-sso/src/main/java/com/swj/itoken/service/sso/ServiceSSOApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.swj.itoken")
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
