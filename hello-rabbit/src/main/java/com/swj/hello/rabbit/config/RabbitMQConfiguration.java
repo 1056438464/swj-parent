38cc238b70b4fa8adaf6d91c74d15cc20394af45
jwsssssssss <1056438464@qq.com>
2019-07-31 22:52:36 +0800

    rabbitmq

---
+++ hello-rabbit/src/main/java/com/swj/hello/rabbit/config/RabbitMQConfiguration.java
@@ -0,0 +1,22 @@
+package com.swj.hello.rabbit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+/**
+ * 队列配置
+ * <p>Title: RabbitMQConfiguration</p>
+ * <p>Description: </p>
+ *
+ * @author Lusifer
+ * @version 1.0.0
+ * @date 2018/5/1 22:39
+ */
+@Configuration
+public class RabbitMQConfiguration {
+    @Bean
+    public Queue queue() {
+        return new Queue("helloRabbit");
+    }
+}
